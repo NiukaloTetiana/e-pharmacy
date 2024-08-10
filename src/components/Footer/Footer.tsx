8941088400ab0f3114763f1b84241a4185dc7a4b
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-08-10 23:45:19 +0300

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,13 +8,13 @@ export const Footer = () => {
           navClass="flex flex-col md:flex-row md:items-center lg:items-start md:justify-between lg:justify-start mb-[80px] md:mb-[20px] lg:gap-[254px]"
           logoClass="flex gap-[12px] md:gap-[14px] md:justify-center items-center text-white logo mb-[20px] md:mb-0"
           logoIconClass="stroke-[#F7F8FA] fill-[#59b17a] md:size-[44px]"
-          className="flex w-[245px] mb-[40px] md:hidden"
+          className="flex w-[261px] mb-[40px] md:hidden"
           linkListClass="flex gap-[32px] lg:gap-[50px]"
           linkClasses={["link-footer", "link-footer", "link-footer"]}
         />
 
         <div className="flex items-start justify-between md:mb-[88px] lg:mb-[64px]">
-          <Description className="hidden md:flex md:w-[261px]" />
+          <Description className="hidden md:flex md:w-[261px] lg:w-[311px]" />
           <ul className="hidden md:flex gap-[12px] lg:absolute lg:right-[96px] lg:top-0">
             <li className="w-[44px] h-[44px]">
               <a
