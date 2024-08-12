497a29686ab51eddf3e5848ee747a69cebc5441c
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-08-12 22:39:45 +0300

    update Home page and StockList component

--- src/components/StockList/StockList.tsx
+++ src/components/StockList/StockList.tsx
@@ -1,51 +1,53 @@
 export const StockList = () => {
   return (
-    <div className="pt-10 md:pt-[64px] pb-[80px] md:pb-[120px]">
-      <ul className="flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap lg:justify-center gap-[16px] md:gap-[28px]">
-        <li className="li-home">
-          <p className="flex gap-[14px] items-center mb-[14px]">
-            <span className="span-home">1</span>Huge Sale
-          </p>
-          <p className="font-medium text-[24px] md:text-[36px] leading-[1.4] text-[#252539]">
-            70%
-            <button
-              type="button"
-              className="font-normal text-[13px] leading-[1.4] text-[#93939a] ml-[28px]"
-            >
-              Shop now
-            </button>
-          </p>
-        </li>
-        <li className="li-home">
-          <p className="flex gap-[14px] items-center mb-[14px]">
-            <span className="span-home">2</span>Secure delivery
-          </p>
+    <section className="pt-10 md:pt-[64px] pb-[80px] md:pb-[120px]">
+      <div className="container">
+        <ul className="flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap lg:justify-center gap-[16px] md:gap-[28px]">
+          <li className="li-home">
+            <p className="flex gap-[14px] items-center mb-[14px]">
+              <span className="span-home">1</span>Huge Sale
+            </p>
+            <p className="font-medium text-[24px] md:text-[36px] leading-[1.4] text-[#252539]">
+              70%
+              <button
+                type="button"
+                className="font-normal text-[13px] leading-[1.4] text-[#93939a] ml-[28px]"
+              >
+                Shop now
+              </button>
+            </p>
+          </li>
+          <li className="li-home">
+            <p className="flex gap-[14px] items-center mb-[14px]">
+              <span className="span-home">2</span>Secure delivery
+            </p>
 
-          <p className="font-medium text-[24px] md:text-[36px] leading-[1.4] text-[#252539]">
-            100%
-            <button
-              type="button"
-              className="font-normal text-[13px] text-[#93939a] leading-[1.4] ml-[28px]"
-            >
-              Read more
-            </button>
-          </p>
-        </li>
-        <li className="li-home">
-          <p className="flex gap-[14px] items-center mb-[14px]">
-            <span className="span-home">3</span>Off
-          </p>
-          <p className="font-medium text-[24px] md:text-[36px] leading-[1.4] text-[#252539]">
-            35%
-            <button
-              type="button"
-              className="font-normal text-[13px] text-[#93939a] leading-[1.4] ml-[28px]"
-            >
-              Shop now
-            </button>
-          </p>
-        </li>
-      </ul>
-    </div>
+            <p className="font-medium text-[24px] md:text-[36px] leading-[1.4] text-[#252539]">
+              100%
+              <button
+                type="button"
+                className="font-normal text-[13px] text-[#93939a] leading-[1.4] ml-[28px]"
+              >
+                Read more
+              </button>
+            </p>
+          </li>
+          <li className="li-home">
+            <p className="flex gap-[14px] items-center mb-[14px]">
+              <span className="span-home">3</span>Off
+            </p>
+            <p className="font-medium text-[24px] md:text-[36px] leading-[1.4] text-[#252539]">
+              35%
+              <button
+                type="button"
+                className="font-normal text-[13px] text-[#93939a] leading-[1.4] ml-[28px]"
+              >
+                Shop now
+              </button>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,19 +3,19 @@ import { StockList } from "../components";
 const Home = () => {
   return (
     <>
-      <div className="bg-[#59B17A] pt-[191px] pb-[337px] md:pt-[298px] md:pb-[414px] lg:pt-[184px] lg:pb-[302px]">
-        <div className="flex flex-col items-center justify-center bg-img h-[312px] md:h-[508px] lg:w-[749px] mx-auto">
-          <h1 className="w-[293px] md:w-[609px] font-semibold text-[50px] md:text-[74px] leading-[1] text-white">
-            Your medication delivered
-          </h1>
-          <p className="w-[156px] md:w-[207px] font-normal text-[12px] md:text-[16px] leading-[1.25] text-white">
-            Say goodbye to all your healthcare worries with us
-          </p>
+      <section className="bg-[#59B17A] pt-[172px] pb-[246px] md:pt-[161px] md:pb-[258px] lg:pt-[47px] lg:pb-[146px]">
+        <div className="container">
+          <div className="pt-[19px] md:pt-[140px] md:pl-[31px] md:pr-[83px] bg-img h-[312px] w-[331px] sm-max:w-[280px] md:h-[508px] md:w-auto lg:w-[749px] mx-auto">
+            <h1 className="w-[293px] md:w-[609px] font-semibold text-[50px] md:text-[74px] leading-[1] text-white mb-[20px] md:mb-[24px]">
+              Your medication delivered
+            </h1>
+            <p className="w-[156px] md:w-[207px] font-normal text-[12px] md:text-[16px] leading-[1.25] text-white ml-auto">
+              Say goodbye to all your healthcare worries with us
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="container">
-        <StockList />
-      </div>
+      </section>
+      <StockList />
     </>
   );
 };
