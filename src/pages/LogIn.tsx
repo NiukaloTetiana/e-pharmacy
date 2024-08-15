0ba5d864a9b4c75925133b89cc914f696608c5d7
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-08-15 13:07:49 +0300

    Update LogIn.tsx

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -2,7 +2,7 @@ import { TitleAuth } from "../components";
 
 const LogIn = () => {
   return (
-    <div className="container pt-[81px] md:pt-[px] lg:pt-[px]">
+    <div className="container pt-[81px] md:pt-[143px] lg:pt-[201px]">
       <TitleAuth />
     </div>
   );
