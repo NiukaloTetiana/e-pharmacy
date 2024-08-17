ae38b2ecfcbc58dccb7f4179668d26d8f4586d21
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-08-17 12:33:40 +0300

    update ScrollUpButton component

--- src/components/ScrollUpButton/ScrollUpButton.tsx
+++ src/components/ScrollUpButton/ScrollUpButton.tsx
@@ -1,19 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import throttle from "lodash.throttle";
 import { Icon } from "../../components";
 
 export const ScrollUpButton = () => {
   const [isVisible, setIsVisible] = useState(false);
 
+  const scrollYRef = useRef(0);
+
   useEffect(() => {
     const handleScroll = throttle(() => {
-      const scrollY = window.scrollY || document.documentElement.scrollTop;
-      if (scrollY > 400) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
-    }, 200);
+      scrollYRef.current = window.scrollY;
+      setIsVisible(scrollYRef.current > 300);
+    }, 400);
 
     window.addEventListener("scroll", handleScroll);
 
@@ -22,7 +20,7 @@ export const ScrollUpButton = () => {
     };
   }, []);
 
-  const scrollUp = () => {
+  const handleClickBtn = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
@@ -31,7 +29,7 @@ export const ScrollUpButton = () => {
 
   return (
     <button
-      onClick={scrollUp}
+      onClick={handleClickBtn}
       className={`
         flex justify-center items-center fixed right-5 bottom-5 sm-max:bottom- sm-max:right-3 md:right-[35px] md:bottom-[25px] lg:right-[60px] lg:bottom-10 rounded-full w-[38px] h-[70px] sm-max:w-[24px] sm-max:h-[44px] lg:w-[44px] lg:h-[82px] bg-[#59b17a] border-[8px] sm-max:border-[6px] border-white z-[49] shadow-scroll hover:-translate-y-2 focus:-translate-y-2 transition-transform duration-300 ${
           !isVisible ? "scale-0" : "scale-1"
