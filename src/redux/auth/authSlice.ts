610e461f1669b7e14e897ca89637ea99f038faaf
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-08-22 22:21:08 +0300

    update productSlice

--- src/redux/auth/authSlice.ts
+++ src/redux/auth/authSlice.ts
@@ -1,4 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, isAnyOf } from "@reduxjs/toolkit";
+import {
+  loginUser,
+  logoutUser,
+  refreshUser,
+  registerUser,
+} from "./authOperations";
 
 export interface IAuthSlice {
   user: { name: string | null; email: string | null };
@@ -6,7 +12,6 @@ export interface IAuthSlice {
   refreshToken: string | null;
   isLoggedIn: boolean;
   isRefreshing: boolean;
-  isLoading: boolean;
 }
 
 const initialState: IAuthSlice = {
@@ -15,10 +20,67 @@ const initialState: IAuthSlice = {
   refreshToken: null,
   isLoggedIn: false,
   isRefreshing: false,
-  isLoading: false,
 };
 
-const authSlice = createSlice({ name: "auth", initialState, reducers: {} });
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.isRefreshing = false;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.accessToken = action.payload.accessToken;
+        state.refreshToken = action.payload.refreshToken;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.accessToken = action.payload.accessToken;
+        state.refreshToken = action.payload.refreshToken;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(logoutUser.fulfilled, () => {
+        return initialState;
+      })
+      .addMatcher(
+        isAnyOf(
+          registerUser.pending,
+          loginUser.pending,
+          refreshUser.pending,
+          logoutUser.pending
+        ),
+        (state) => {
+          state.isRefreshing = true;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          registerUser.rejected,
+          loginUser.rejected,
+          refreshUser.rejected,
+          logoutUser.rejected
+        ),
+        (state) => {
+          state.isRefreshing = false;
+        }
+      );
+  },
+
+  selectors: {
+    selectUser: (state) => state.user,
+    selectIsLoggedIn: (state) => state.isLoggedIn,
+    selectIsRefreshing: (state) => state.isRefreshing,
+  },
+});
 
 export const authReducer = authSlice.reducer;
+export const { selectUser, selectIsLoggedIn, selectIsRefreshing } =
+  authSlice.selectors;
 export type AuthState = ReturnType<typeof authReducer>;
